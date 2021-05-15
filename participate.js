89322f68ade35c95def4cba00cd5c3850117918e
somnium421 <somnium421@kaist.ac.kr>
2021-05-15 20:40:00 +0900

    why added tag no toggle???

--- participate.js
+++ participate.js
@@ -1,21 +1,7 @@
-//This is for new meeting creation page
-
-console.log("hello world")
-
-$("#Txt_Date").datepicker({
-    format: 'd-M-yyyy',
-    inline: false,
-    lang: 'en',
-    step: 5,
-    multidate: 5,
-    closeOnDateSelect: true
-});
-
 var navListItems = $('div.setup-panel div a'),
     allWells = $('.setup-content'),
     allNextBtn = $('.nextBtn'),
     allPrevBtn = $('.prevBtn');
-
     allWells.hide();
 
 navListItems.click(function (e) {
@@ -60,3 +46,46 @@ allNextBtn.click(function(){
 });
 
 $('div.setup-panel div a.btn-light').trigger('click');
+
+function toggle(target) {
+    if (target.data('selected')) {
+        target.removeClass('btn-primary');      
+        target.addClass('btn-outline-primary');  
+    }
+    else {
+        target.removeClass('btn-outline-primary');
+        target.addClass('btn-primary');
+    }
+    target.data('selected', !target.data('selected'));
+    return !target.data('selected');
+}
+
+$('#location-add').click(function(event) {
+    console.log('hello');
+    if (toggle($(event.target))) $('#location-input').addClass('d-none');
+    else $('#location-input').removeClass('d-none');
+});
+
+$('#location-add-tag').click(function() {
+    if ($('#location-input-text').val() != '') {
+        $('#location-add').before(`<button type="button" class="btn btn-primary rounded-pill me-2 selectable" data-selected=true>${$('#location-input-text').val()}</button>`);
+        $('#location-input-text').val('');
+    }
+});
+
+$('#activity-add').click(function(event) {
+    console.log('hello');
+    if (toggle($(event.target))) $('#activity-input').addClass('d-none');
+    else $('#activity-input').removeClass('d-none');
+});
+
+$('#activity-add-tag').click(function() {
+    if ($('#activity-input-text').val() != '') {
+        $('#activity-add').before(`<button type="button" class="btn btn-primary rounded-pill me-2 selectable" data-selected=true>${$('#activity-input-text').val()}</button>`);
+        $('#activity-input-text').val('');
+    }
+});
+
+$('.selectable').click(function(event) {
+    toggle($(event.target));
+});
