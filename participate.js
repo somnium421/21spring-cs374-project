eebb279e493c1f57f5bfe4d4b7e0e2c44f7cd733
somnium421 <somnium421@kaist.ac.kr>
2021-05-17 17:15:34 +0900

    map added

--- participate.js
+++ participate.js
@@ -90,3 +90,33 @@ $('.selectable').click(function(event) {
     toggle($(event.target));
 });
 
+var map = new naver.maps.Map("map", {
+    center: new naver.maps.LatLng(37.3595316, 127.1052133),
+    zoom: 15
+});
+
+var infoWindow = new naver.maps.InfoWindow({
+    anchorSkew: true
+});
+  
+function searchAddressToCoordinate(address) {
+    naver.maps.Service.geocode({query: address}, function(status, response) {
+        if (status === naver.maps.Service.Status.ERROR) {
+            if (!address) return alert('Geocode Error, Please check address');
+            return alert('Geocode Error, address:' + address);
+        }
+        if (response.v2.meta.totalCount === 0) return alert('No result.');
+        var htmlAddresses = [],
+            item = response.v2.addresses[0],
+            point = new naver.maps.Point(item.x, item.y);
+        if (item.roadAddress) htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
+        infoWindow.setContent([
+            '<div style="padding:10px;min-width:200px;line-height:150%;">',
+            '<h4 style="margin-top:5px;">검색 주소 : '+ address +'</h4><br />',
+            htmlAddresses.join('<br />'),
+            '</div>'
+        ].join('\n'));
+        map.setCenter(point);
+        infoWindow.open(map, point);
+    });
+}
