3b8459e96861c0575793687cbe8e3771c69f669d
Hye Soo Park <hyehye@kaist.ac.kr>
2021-05-18 09:57:18 +0900

    코드 입력 페이지 하는중

--- sign-in.js
+++ sign-in.js
@@ -2,6 +2,28 @@ var userId;
 var userPw;
 
 
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+  
+
+
 $(document).ready(function(){
     $("#sign-in").click(()=>{
         userId = $("#userId").val();
