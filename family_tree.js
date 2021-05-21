170d5d2757cfc93252fe5608cbdf81c241334302
Hye Soo Park <hyehye@kaist.ac.kr>
2021-05-22 05:29:44 +0900

    난수

--- family_tree.js
+++ family_tree.js
@@ -156,22 +156,26 @@ function addChild(nodeId){
 
 var randomNum = {};
 //0~9까지의 난수
-randomNum.random = function(n1, n2) {
-    return parseInt(Math.random() * (n2 -n1 +1)) + n1;
+randomNum.random = function() {
+    var int = Math.floor(Math.random()*37);
+    if (int > 9) return String.fromCharCode(int + 55);
+    return String(int);
 };
 //인증번호를 뽑을 난수 입력 n 5이면 5자리
 randomNum.authNo= function(n) {
     var value = "";
     for(var i=0; i<n; i++){
-        value += randomNum.random(0,9);
+        value += randomNum.random();
     }
     return value;
 };
 //화면에 번호 출력
-randomNum.printRandom =function(data,num) {
-    document.getElementById(data).innerHTML = randomNum.authNo(num);
+randomNum.printRandom = function(data, num) {
+    $(data).append(`<span class="text-primary"> ${randomNum.authNo(num)}</span>`);
+    console.log(randomNum.authNo(num));
 };
 
+randomNum.printRandom("#family-code-auth", 5);
 
 $(document).ready(function(){
     console.log($("#tree"));
