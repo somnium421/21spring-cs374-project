02e84d81161bb1f80161590cf90f54f8aab2a608
somnium421 <somnium421@kaist.ac.kr>
2021-05-22 11:22:43 +0900

    load user submitted data, done db, remove error fixed

--- participate.js
+++ participate.js
@@ -1,13 +1,13 @@
 //import firebase from "firebase";
 
-const familyCode = "00AB8", meetingNumber = 0, userID = 0;
+const familyCode = "00AB8", meetingNumber = 0, userID = 1;
 var meetings, members, docID;
 
 var answer = {
     place: [],
     activity: [],
     accommodation: [],
-    departure: undefined,
+    departure: null,
     transportation: [],
 };
 
@@ -21,8 +21,8 @@ function toggle(target) {
     if (target.data('selected')) {
         target.removeClass('btn-primary');      
         target.addClass('btn-outline-primary');  
-        if (target.parent().attr('id') == 'place-tags' && target.html() != '+') answer.place.splice(answer.place.indexOf(target.html()));
-        if (target.parent().attr('id') == 'activity-tags' && target.html() != '+') answer.activity.splice(answer.activity.indexOf(target.html()));
+        if (target.parent().attr('id') == 'place-tags' && target.html() != '+') answer.place.splice(answer.place.indexOf(target.html()), 1);
+        if (target.parent().attr('id') == 'activity-tags' && target.html() != '+') answer.activity.splice(answer.activity.indexOf(target.html()), 1);
     }
     else {
         target.removeClass('btn-outline-primary');
@@ -42,27 +42,54 @@ $(document).ready(function() {
             docID = doc.id;
             meetings = doc.data().meetings;
             members = doc.data().members;
+            console.log(meetings);
+
             if (meetings[meetingNumber].isPrivate) $('#is-private').append('<span class="text-muted" style="font-size: smaller;"><i class="fas fa-lock me-1"></i> 비공개</span>')
             else $('#is-private').append('<span class="text-muted" style="font-size: smaller;"><i class="fas fa-globe-asia me-1"></i> 공개</span>')
+
             $('#meeting-name').text(meetings[meetingNumber].name);
+
             $('#meeting-description').text(meetings[meetingNumber].description);
+
             for (var participant of meetings[meetingNumber].participants) {
                 $('#meeting-participants').append(` <a href="#" class="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title=${participant.name}>
                                                         <img src="${members[participant.id].img}" style="width:30px;height:30px;border-radius:70%;opacity:${(Math.random(1)<0.5)?0.5:1}"></img>
                                                     </a>`);
             }
-            var meetingTags
-            if (meetings.noMorePlace) 
-            for (var place of meetings[meetingNumber].place) {
-                $('#place-tags').append(`<button type="button" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill selectable data-selected=false">${place}</button>`)
+
+            var meetingTags;
+            if (meetings[meetingNumber].meetingPeriod.day == 0) meetingTags = `<span class="badge rounded-pill bg-light text-dark">${meetings[meetingNumber].meetingPeriod.hour}시간</span> <span style="font-size: smaller;">동안</span>`;
+            else if (meetings[meetingNumber].meetingPeriod.day == 1) meetingTags = `<span class="badge rounded-pill bg-light text-dark">하루</span> <span style="font-size: smaller;">종일</span>`;
+            else meetingTags = `<span class="badge rounded-pill bg-light text-dark">${meetings[meetingNumber].meetingPeriod.day-1}박 ${meetings[meetingNumber].meetingPeriod.day}일</span> <span style="font-size: smaller;">동안</span> `
+            if (meetings[meetingNumber].noMorePlace) {
+                var tmp = meetings[meetingNumber].place.map((x) => `<span class="badge rounded-pill bg-light text-dark">${x}</span>`).join('');
+                meetingTags += tmp + '<span style="font-size: smaller;">에서</span>';
             }
-            $('#place-tags').append(`<button type="button" id="place-input-reveal" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill data-selected=false">+</button>`);
-            for (var activity of meetings[meetingNumber].activity) {
-                $('#activity-tags').append(`<button type="button" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill selectable data-selected=false">${activity}</button>`)
+            if (meetings[meetingNumber].noMoreActivity) {
+                var tmp = meetings[meetingNumber].activity.map((x) => `<span class="badge rounded-pill bg-light text-dark">${x}</span>`).join('');
+                meetingTags += tmp;
             }
-            $('#activity-tags').append(`<button type="button" id="activity-input-reveal" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill data-selected=false">+</button>`);
-           
-            bindEvents();
+            $('#meeting-tags').append(meetingTags);
+
+            var placeSet = new Set(meetings[meetingNumber].place), activitySet = new Set(meetings[meetingNumber].activity);
+            db.collection('families').doc(docID).collection('answers').where('meetingNumber', '==', meetingNumber)
+            .get()
+            .then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    for (var place of doc.data().place) placeSet.add(place);
+                    for (var activity of doc.data().activity) activitySet.add(activity);
+                });
+                for (var place of placeSet) {
+                    $('#place-tags').append(`<button type="button" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill selectable data-selected=false">${place}</button>`)
+                }
+                if (!meetings[meetingNumber].noMorePlace) $('#place-tags').append(`<button type="button" id="place-input-reveal" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill data-selected=false">+</button>`);
+    
+                for (var activity of activitySet) {
+                    $('#activity-tags').append(`<button type="button" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill selectable data-selected=false">${activity}</button>`)
+                }
+                if (!meetings[meetingNumber].noMoreActivity) $('#activity-tags').append(`<button type="button" id="activity-input-reveal" class="tag btn btn-outline-primary btn-sm mt-2 mx-1 rounded-pill data-selected=false">+</button>`);
+                bindEvents();
+            })
         });
     });
 });
@@ -105,36 +132,8 @@ function bindEvents() {
         }
         if (isValid) nextStepWizard.removeAttr('disabled').trigger('click');
         
-        if (answer.accommodation=[]) for (var checked of $("#accommodation input[type='checkbox']:checked")) answer.accommodation.push($(checked).attr('id'));
-        if (answer.transportation=[]) for (var transportation of $("#transportation input[type='radio']:checked")) answer.transportation.push($(transportation).attr('id'));
-        
-        //modify!!!!
-        meetings[meetingNumber].participants[userID]['answer'] = answer;
-        console.log(meetings);
-        db.collection('families').doc(docID).update({meetings: meetings});
-        
-        /*var update = {};
-        update[`meetings[${meetingNumber}].participants[${userID}].answer`] = answer;
-        console.log(update);*/
-        /*
-        db.collection('families').doc(docID).update({
-            "meetings[0].participants[0].answer": answer
-        });*/
-        /*
-        db.collection('families').where('code', '==', familyCode)
-            .get()
-            .then((snapshot) => {
-                console.log('hello');
-                snapshot.forEach((doc) => {
-                    console.log(doc.id);
-                    update = doc.data().meetings;
-                    update[meetingNumber].participants[userID]['answer'] = answer;
-                    console.log(update);
-                    //update[`meetings[${meetingNumber}].participants[${userID}]`] = doc.data().meetings[meetingNumber].participants[userID];
-                    //update[`meetings[${meetingNumber}].participants[${userID}].answer`] = answer;
-                })
-                db.collection('families').where('code', '==', familyCode).get().update(update);
-            })*/
+        if (answer.accommodation == []) for (var checked of $("#accommodation input[type='checkbox']:checked")) answer.accommodation.push($(checked).attr('id'));
+        if (answer.transportation == []) for (var transportation of $("#transportation input[type='radio']:checked")) answer.transportation.push($(transportation).attr('id'));
     });
 
     $('div.setup-panel div a.btn-light').trigger('click');
@@ -144,7 +143,7 @@ function bindEvents() {
         if ($('#place-input').val() !== "") {
             var inputText = $('#place-input').val();
             answer.place.push(inputText);
-            $('#place-input-reveal').before(`<button type="tag" class="tag btn btn-primary btn-sm mt-2 mx-1 rounded-pill" data-place-activity="place">${inputText}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x remove-tag" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button>`);
+            $('#place-input-reveal').before(`<button type="button" class="tag btn btn-primary btn-sm mt-2 mx-1 rounded-pill" data-place-activity="place">${inputText}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x remove-tag" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button>`);
             $('#place-input').val("");
         }
     });
@@ -159,7 +158,7 @@ function bindEvents() {
         if ($('#activity-input').val() !== "") {
             var inputText = $('#activity-input').val();
             answer.activity.push(inputText);
-            $('#activity-input-reveal').before(`<button type="tag" class="tag btn btn-primary btn-sm mt-2 mx-1 rounded-pill" data-place-activity="activity">${inputText}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x remove-tag" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button>`);
+            $('#activity-input-reveal').before(`<button type="button" class="tag btn btn-primary btn-sm mt-2 mx-1 rounded-pill" data-place-activity="activity">${inputText}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x remove-tag" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button>`);
             $('#activity-input').val("");
         }
     });
@@ -177,6 +176,18 @@ function bindEvents() {
         getAddr();
     });
 
+    $('#submit-button').click(function() {
+        db.collection('families').doc(docID).collection('answers').add({
+            meetingNumber: meetingNumber,
+            userID: userID,
+            place: answer.place,
+            activity: answer.activity,
+            accommodation: answer.accommodation,
+            departure: answer.departure,
+            transportation: answer.transportation
+        });
+    });
+
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
@@ -187,21 +198,17 @@ function bindEvents() {
 $(document).on('click', '.remove-tag', function(e){
     e.preventDefault();
     if ($(e.target)[0].outerHTML.slice(1,5)==="path") {
-        console.log('1')
-        console.log($(e.target).data('place-activity'))
-        if ($(e.target).data('place-activity') == "place") answer.place.splice(answer.place.indexOf($(e.target).text()));
-        if ($(e.target).data('place-activity') == "activity") answer.activity.splice(answer.activity.indexOf($(e.target).text()));    
+        if ($(e.target).parent().parent().data('place-activity') == "place") answer.place.splice(answer.place.indexOf($(e.target).parent().parent().text()), 1);
+        if ($(e.target).parent().parent().data('place-activity') == "activity") answer.activity.splice(answer.activity.indexOf($(e.target).parent().parent().text()), 1);   
         $(e.target).parent().parent().remove();
     }
     else {
-        console.log('2')
-        console.log($(e.target).parent().data('place-activity'))
-        if ($(e.target).parent().data('place-activity') == "place") answer.place.splice(answer.place.indexOf($(e.target).parent().text()));
-        if ($(e.target).parent().data('place-activity') == "activity") answer.activity.splice(answer.activity.indexOf($(e.target).parent().text()));
+        console.log(answer.place);
+        if ($(e.target).parent().data('place-activity') == "place") answer.place.splice(answer.place.indexOf($(e.target).parent().text()), 1);
+        if ($(e.target).parent().data('place-activity') == "activity") answer.activity.splice(answer.activity.indexOf($(e.target).parent().text()), 1);
         $(e.target).parent().remove();
     }
 })
-
 
 var map, marker, currPos;
 navigator.geolocation.getCurrentPosition(onSuccessGeolocation, onErrorGeolocation);
@@ -254,6 +261,7 @@ function getAddr(){
         alert('No result');
     });
 }
+
 var prevLocation;
 function locationClick(event) {
     if (prevLocation) {
