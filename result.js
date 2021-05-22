11755c86bf621f5e856c1b9afcca2b60547234e4
somnium421 <somnium421@kaist.ac.kr>
2021-05-22 12:23:44 +0900

    like done

--- result.js
+++ result.js
@@ -5,27 +5,9 @@ const chat = [
     {id: 7, text: '근데 가평 이번 주에 비 온대ㅠㅜ', time: new Date(2021,4,20,19,24,30,0), like: [3, 8]}
 ]
 
-const locationData = [
-    {name: '<a data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="아빠, 작은아빠, 누나">가평</a>', weight: 3, tooltip: '엄마, 누나, 나'},
-    {name: '홍대', weight: 2, tooltip: '누나, 나'},
-    {name: '동해바다', weight: 2, tooltip: '할머니, 아빠'},
-    {name: '광안리', weight: 3, tooltip: '아빠, 작은아빠, 누나'},
-    {name: '대관령', weight: 1, tooltip: '나'},
-    {name: '경주', weight: 1, tooltip: '할머니'},
-]
+const placeData = [], activityData = [];
 
-const activityData = [
-    {name: '호캉스', weight: 5, tooltip: '엄마, 누나, 나, 아빠, 할머니'},
-    {name: '캠핑', weight: 2, tooltip: '누나, 나'},
-    {name: '수다 떨기', weight: 2, tooltip: '할머니, 아빠'},
-    {name: '등산', weight: 3, tooltip: '아빠, 작은아빠, 누나'},
-    {name: '간장게장 먹기', weight: 1, tooltip: '나'},
-]
-
-let a='아빠, 작은아빠, 누나';
-// acitivitiData.filter()
-
-let locationOptions = {
+let placeOptions = {
     container: {
     },
     tag: {
@@ -33,7 +15,7 @@ let locationOptions = {
         maxFontSize: 20,
         format: '{tag.name}'
     },
-    data: locationData
+    data: placeData
 }
 
 let activityOptions = {
@@ -42,18 +24,17 @@ let activityOptions = {
     tag: {
         minFontSize: 12,
         maxFontSize: 20,
-        format: `<a>{tag.name}</a>`
+        format: '{tag.name}'
     },
     data: activityData
 }
 
 function resize() {
-    locationOptions.container.width = $('#location-card').width();
+    placeOptions.container.width = $('#place-card').width();
     activityOptions.container.width = $('#activity-card').width();
-    $('#locationCloud').tagCloud(locationOptions);
+    $('#placeCloud').tagCloud(placeOptions);
     $('#activityCloud').tagCloud(activityOptions);
 }
-resize();
 
 var map = new naver.maps.Map("map", {
     center: new naver.maps.LatLng(35.8, 127.51),
@@ -101,7 +82,6 @@ function tooltipSet() {
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
-
 }
 
 function bindEvents() {
@@ -116,13 +96,26 @@ function bindEvents() {
         $('#chat-input').val('')
         drawChat(chat.length-1);
     });
-    $(`.bi-heart-fill`).click((event) => {
-        $(event.target).css({fill: '#FF9999'});
-        console.log($(event.target));
-    });
 
     tooltipSet();
 }
+
+$(document).on('click', '.bi-heart-fill', function(e){
+    e.preventDefault();
+    if ($(e.target)[0].outerHTML.slice(1,5)==="path") {
+        var idx = $(e.target).parent().data('idx');
+        if ($(e.target).css('fill') != "rgb(255, 153, 153)") { // unclicked
+            $(e.target).css({fill: 'rgb(255, 153, 153)'});
+            chat[idx].like.push(userID);
+            $(e.target).parent().parent().attr('data-bs-original-title', chat[idx].like.map((id) => members[id].name).join(', '));
+        }
+        else { // clicked
+            $(e.target).css({fill: 'rgb(255, 192, 203)'});
+            chat[idx].like.splice(chat[idx].like.indexOf(userID), 1);
+            $(e.target).parent().parent().attr('data-bs-original-title', chat[idx].like.map((id) => members[id].name).join(', '));
+        }
+    }
+})
 
 function timeCalculate(time) {
     return `${time.getMonth()+1}월 ${time.getDate()}일 ${time.getHours()}시 ${time.getMinutes()}분` 
@@ -143,7 +136,7 @@ function drawChat(idx) {
                 <div class="received_withd_msg">
                     <p style="float:left">${chatElem.text}</p>
                     <a style="float:right" class="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="${chatElem.like.map((id) => members[id].name).join(', ')}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffc0cb" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffc0cb" class="bi bi-heart-fill" viewBox="0 0 16 16" data-idx="${idx}">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                         </svg>
                     </a>
@@ -158,7 +151,7 @@ function drawChat(idx) {
             <div class="sent_msg">
                 <p style="float:left">${chatElem.text}</p>
                 <a style="float:right" class="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="left" title="" data-bs-original-title="${chatElem.like.map((id) => members[id].name).join(', ')}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffc0cb" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffc0cb" class="bi bi-heart-fill" viewBox="0 0 16 16" data-idx="${idx}">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                     </svg>
                 </a>
@@ -169,6 +162,43 @@ function drawChat(idx) {
     tooltipSet();
 }
 
+function processData() {
+    db.collection('families').doc(docID).collection('answers').where('meetingNumber', '==', meetingNumber)
+    .get()
+    .then((snapshot) => {
+        var placeDict = {}, activityDict = {};
+        snapshot.forEach((doc) => {
+            console.log(doc.data());
+            for (var place of doc.data().place) {
+                if (place in placeDict) placeDict[place].push(doc.data().userID);
+                else placeDict[place] = [doc.data().userID];
+            }
+            for (var activity of doc.data().activity) {
+                if (activity in activityDict) activityDict[plactivityace].push(doc.data().userID);
+                else activityDict[activity] = [doc.data().userID];
+            }
+        });
+        for (var place in placeDict) {
+            var tooltipTitle = place
+            placeData.push({
+                name: `<a data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="${placeDict[place].map((id) => members[id].name).join(', ')}">${place}</a>`,
+                weight: placeDict[place].length
+            });
+        }
+        
+        for (var activity in activityDict) {
+            var tooltipTitle = place
+            activityData.push({
+                name: `<a data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="${activityDict[activity].map((id) => members[id].name).join(', ')}">${activity}</a>`,
+                weight: activityDict[activity].length
+            });
+        }
+        resize();
+        tooltipSet();
+        console.log(placeData, activityData);
+    })
+}
+
 $(document).ready(function() {
     db.collection('families').where('code', '==', familyCode)
     .get()
@@ -177,6 +207,37 @@ $(document).ready(function() {
             docID = doc.id;
             meetings = doc.data().meetings;
             members = doc.data().members;
+
+            processData();
+
+            console.log(meetings);
+
+            if (meetings[meetingNumber].isPrivate) $('#is-private').append('<span class="text-muted" style="font-size: smaller;"><i class="fas fa-lock me-1"></i> 비공개</span>')
+            else $('#is-private').append('<span class="text-muted" style="font-size: smaller;"><i class="fas fa-globe-asia me-1"></i> 공개</span>')
+
+            $('#meeting-name').text(meetings[meetingNumber].name);
+
+            $('#meeting-description').text(meetings[meetingNumber].description);
+
+            for (var participant of meetings[meetingNumber].participants) {
+                $('#meeting-participants').append(` <a href="#" class="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title=${participant.name}>
+                                                        <img src="${members[participant.id].img}" style="width:30px;height:30px;border-radius:70%;opacity:${(Math.random(1)<0.5)?0.5:1}"></img>
+                                                    </a>`);
+            }
+
+            var meetingTags;
+            if (meetings[meetingNumber].meetingPeriod.day == 0) meetingTags = `<span class="badge rounded-pill bg-light text-dark">${meetings[meetingNumber].meetingPeriod.hour}시간</span> <span style="font-size: smaller;">동안</span>`;
+            else if (meetings[meetingNumber].meetingPeriod.day == 1) meetingTags = `<span class="badge rounded-pill bg-light text-dark">하루</span> <span style="font-size: smaller;">종일</span>`;
+            else meetingTags = `<span class="badge rounded-pill bg-light text-dark">${meetings[meetingNumber].meetingPeriod.day-1}박 ${meetings[meetingNumber].meetingPeriod.day}일</span> <span style="font-size: smaller;">동안</span> `
+            if (meetings[meetingNumber].noMorePlace) {
+                var tmp = meetings[meetingNumber].place.map((x) => `<span class="badge rounded-pill bg-light text-dark">${x}</span>`).join('');
+                meetingTags += tmp + '<span style="font-size: smaller;">에서</span>';
+            }
+            if (meetings[meetingNumber].noMoreActivity) {
+                var tmp = meetings[meetingNumber].activity.map((x) => `<span class="badge rounded-pill bg-light text-dark">${x}</span>`).join('');
+                meetingTags += tmp;
+            }
+            $('#meeting-tags').append(meetingTags);
         });
         for (var i=0; i<chat.length; i++) drawChat(i);
         bindEvents();
