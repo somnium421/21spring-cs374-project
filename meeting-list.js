6586fcd2185b683c78a195c0d467cba5278fab43
Hye Soo Park <hyehye@kaist.ac.kr>
2021-05-23 00:12:40 +0900

    trash

---
+++ meeting-list.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('head').append('<style type="text/css"> #meeting-list {height: ' + ($('.card-body').height()) + 'px;}</style>');
+
+    if ($('.meeting-card').css("display") === "none") $('.meeting-card').show();
+});
+
+$(document).ready(function () {
+    $('head').append('<style type="text/css"> #meeting-list {height: ' + ($('.card-body').height()) + 'px;}</style>');
+
+    if ($('.meeting-card').css("display") === "none") $('.meeting-card').show();
+});
