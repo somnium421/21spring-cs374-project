15b9c13ef3a32edfd4117fdbc7abf27df29dfbe7
somnium421 <somnium421@kaist.ac.kr>
2021-05-23 20:01:03 +0900

    db done, href undone

--- js/login.js
+++ js/login.js
@@ -1,21 +1,41 @@
-const dbIDPW = [];
+const dbData = [];
 var userID, userPw;
 
 $(document).ready(function(){
     db.collection('users').get()
     .then((snapshot) => {
         snapshot.forEach((doc) => {
-            dbIDPW.push({
-                id: doc.data().id,
-                pw: doc.data().pw
-            })
+            dbData.push(doc.data());
         });
-        console.log(dbIDPW);
+        console.log(dbData);
     });
 })
 
 $('#login').click(() => {
-    userId = $("#userId").val();
-    userPw = $("#userPw").val();
-    userID = 'hyehye';
+    userID = $("#userID").val();
+    userPW = $("#userPW").val();
+    console.log(userID, userPW);
+    if (userID == '' || userPW == '') {
+        alert('빈칸을 모두 입력해주세요')
+    }
+    else {
+        var noSuchID = true;
+        for (var user of dbData) {
+            if (user.id == userID) {
+                noSuchID = false;
+                if (user.pw == userPW) {
+                    alert('OK')
+                }
+                else {
+                    $('#userPW').val('')
+                    alert('비밀번호가 틀렸습니다. 다시 입력해주세요')
+                }
+            }
+        }
+        if (noSuchID) {
+            $('#userID').val('')
+            $('#userPW').val('')
+            alert('존재하지 않는 아이디입니다. 다시 입력해주세요')
+        }
+    }
 })
