9d91af226f3df39e7d8af4c5d0c08f6487790406
Hye Soo Park <hyehye@kaist.ac.kr>
2021-05-23 19:08:33 +0900

    home changing

--- js/meeting-list.js
+++ js/meeting-list.js
@@ -1,11 +1,93 @@
-$(document).ready(function () {
-    $('head').append('<style type="text/css"> #meeting-list {height: ' + ($('.card-body').height()) + 'px;}</style>');
+const familyCode = "00AB8", meetingNumber = 0, userID = 0;
+const placeData = [], activityData = [], markers = [], infoWindows = [], latlngs = [];
+var meetings, members, chats, docID, answerID;
 
-    if ($('.meeting-card').css("display") === "none") $('.meeting-card').show();
-});
 
-$(document).ready(function () {
-    $('head').append('<style type="text/css"> #meeting-list {height: ' + ($('.card-body').height()) + 'px;}</style>');
 
-    if ($('.meeting-card').css("display") === "none") $('.meeting-card').show();
+// $(document).ready(function () {
+//     $('head').append('<style type="text/css"> #meeting-list {height: ' + ($('.card-body').height()) + 'px;}</style>');
+
+//     if ($('.meeting-card').css("display") === "none") $('.meeting-card').show();
+// });
+
+// $(document).ready(function () {
+//     $('head').append('<style type="text/css"> #meeting-list {height: ' + ($('.card-body').height()) + 'px;}</style>');
+
+//     if ($('.meeting-card').css("display") === "none") $('.meeting-card').show();
+// });
+
+
+function processData() {
+    db.collection('families').doc(docID).collection('answers').where('meetingNumber', '==', meetingNumber)
+    .get()
+    .then((snapshot) => {
+        var placeDict = {}, activityDict = {};
+        const latlngs = [];
+        snapshot.forEach((doc) => {
+            for (var place of doc.data().place) {
+                if (place in placeDict) placeDict[place].push(doc.data().userID);
+                else placeDict[place] = [doc.data().userID];
+            }
+            for (var activity of doc.data().activity) {
+                if (activity in activityDict) activityDict[activity].push(doc.data().userID);
+                else activityDict[activity] = [doc.data().userID];
+            }
+            const find = latlngs.find(latlng => latlng.position[0] == doc.data().departure[1] && latlng.position[1] == doc.data().departure[0]);
+            if (find) find.id.push(doc.data().userID);
+            else {
+                latlngs.push({
+                    position: [doc.data().departure[1], doc.data().departure[0]],
+                    id: [doc.data().userID]
+                })
+            }
+        });
+        
+
+    })
+}
+
+
+$(document).ready(function() {
+    db.collection('families').where('code', '==', familyCode)
+    .get()
+    .then((snapshot) => {
+        snapshot.forEach((doc) => {
+            docID = doc.id;
+            meetings = doc.data().meetings;
+            //console.log(meetings);
+            members = doc.data().members;
+
+            processData();
+
+
+            if (meetings[meetingNumber].isPrivate) $('#is-private').append('<span class="text-muted" style="font-size: smaller;"><i class="fas fa-lock me-1"></i> 비공개</span>')
+            else $('#is-private').append('<span class="text-muted" style="font-size: smaller;"><i class="fas fa-globe-asia me-1"></i> 공개</span>')
+
+            $('#meeting-name').text(meetings[meetingNumber].name);
+
+            $('#meeting-description').text(meetings[meetingNumber].description);
+
+            for (var participant of meetings[meetingNumber].participants) {
+                $('#meeting-participants').append(` <a href="#" class="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title=${participant.name}>
+                                                        <img src="${members[participant.id].img}" style="width:30px;height:30px;border-radius:70%;opacity:${(Math.random(1)<0.5)?0.5:1}"></img>
+                                                    </a>`);
+            }
+
+            var meetingTags;
+            if (meetings[meetingNumber].meetingPeriod.day == 0) meetingTags = `<span class="badge rounded-pill bg-light text-dark">${meetings[meetingNumber].meetingPeriod.hour}시간</span> <span style="font-size: smaller;">동안</span>`;
+            else if (meetings[meetingNumber].meetingPeriod.day == 1) meetingTags = `<span class="badge rounded-pill bg-light text-dark">하루</span> <span style="font-size: smaller;">종일</span>`;
+            else meetingTags = `<span class="badge rounded-pill bg-light text-dark">${meetings[meetingNumber].meetingPeriod.day-1}박 ${meetings[meetingNumber].meetingPeriod.day}일</span> <span style="font-size: smaller;">동안</span> `
+            if (meetings[meetingNumber].noMorePlace) {
+                var tmp = meetings[meetingNumber].place.map((x) => `<span class="badge rounded-pill bg-light text-dark">${x}</span>`).join('');
+                meetingTags += tmp + '<span style="font-size: smaller;">에서</span>';
+            }
+            if (meetings[meetingNumber].noMoreActivity) {
+                var tmp = meetings[meetingNumber].activity.map((x) => `<span class="badge rounded-pill bg-light text-dark">${x}</span>`).join('');
+                meetingTags += tmp;
+            }
+            $('#meeting-tags').append(meetingTags);
+
+        
+        });
+    });
 });
