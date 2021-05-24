b8c830b949cab38d537b5349009c33835e6e06e4
heeju <heeju@imprimedicine.com>
2021-05-24 18:08:19 +0900

    login validation

--- js/login.js
+++ js/login.js
@@ -15,16 +15,48 @@ $('#login').click(() => {
     userID = $("#userID").val();
     userPW = $("#userPW").val();
     console.log(userID, userPW);
-    if (userID == '' || userPW == '') {
-        alert('빈칸을 모두 입력해주세요')
+    if (userID == '' && userPW == '') {
+        document.getElementById('userID').style = "border: 2px solid #E8ADAA";
+        document.getElementById('userPW').style = "border: 2px solid #E8ADAA";
+        var text = `<small id="loginHelp" class="form-text" style="color: red">아이디와 비밀번호를 입력해주세요.</small>`
+        if ($("#loginHelp").length === 0) $("#userPW").parent().append(text);
+        else {
+            $("#loginHelp").remove();
+            $("#userPW").parent().append(text);}
+    }
+    else if(userID == ''){
+        document.getElementById('userID').style = "border: 2px solid #E8ADAA";
+        document.getElementById('userPW').style = "border: ''";
+        var text = `<small id="loginHelp" class="form-text" style="color: red">아이디를 입력해주세요.</small>`
+        if ($("#loginHelp").length === 0) $("#userPW").parent().append(text);
+        else {
+            $("#loginHelp").remove();
+            $("#userPW").parent().append(text);}
+    }
+    else if (userPW == ''){
+        document.getElementById('userID').style = "border: ''";
+        document.getElementById('userPW').style = "border: 2px solid #E8ADAA";
+        var text = `<small id="loginHelp" class="form-text" style="color: red">비밀번호를 입력해주세요.</small>`
+        if ($("#loginHelp").length === 0) $("#userPW").parent().append(text);
+        else {
+            $("#loginHelp").remove();
+            $("#userPW").parent().append(text);}
     }
     else {
+        document.getElementById('userID').style = "border: ''";
+        document.getElementById('userPW').style = "border: ''";
+        $("#loginHelp").remove();
+
         var noSuchID = true;
         for (var user of dbData) {
             console.log(user);
             if (user.id == userID) {
                 noSuchID = false;
                 if (user.pw == userPW) {
+                    document.getElementById('userID').style = "border: ''";
+                    document.getElementById('userPW').style = "border: ''";
+                    $("#loginHelp").remove();
+
                     localStorage.setItem('family-code', user["family-code"]);
                     localStorage.setItem('family-id', user["family-id"]);
                     localStorage.setItem('id', user["id"]);
@@ -33,14 +65,28 @@ $('#login').click(() => {
                 }
                 else {
                     $('#userPW').val('')
-                    alert('비밀번호가 틀렸습니다. 다시 입력해주세요')
+                    //alert('비밀번호가 틀렸습니다. 다시 입력해주세요')
+                    document.getElementById('userPW').style = "border: 2px solid #E8ADAA";
+                    var text = `<small id="loginHelp" class="form-text" style="color: red">비밀번호가 틀렸습니다. 다시 입력해주세요</small>`
+                    if ($("#loginHelp").length === 0) $("#userPW").parent().append(text);
+                    else {
+                        $("#loginHelp").remove();
+                        $("#userPW").parent().append(text);
+                    }
                 }
             }
         }
         if (noSuchID) {
             $('#userID').val('')
             $('#userPW').val('')
-            alert('존재하지 않는 아이디입니다. 다시 입력해주세요')
+            //alert('존재하지 않는 아이디입니다. 다시 입력해주세요')
+            document.getElementById('userID').style = "border: 2px solid #E8ADAA";
+            var text = `<small id="loginHelp" class="form-text" style="color: red">존재하지 않는 아이디입니다.다시 입력해주세요</small>`
+            if ($("#loginHelp").length === 0) $("#userPW").parent().append(text);
+            else {
+                $("#loginHelp").remove();
+                $("#userPW").parent().append(text);
+            }
         }
     }
 })
