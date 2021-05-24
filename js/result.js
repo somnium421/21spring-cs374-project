58852cdb8b83463c29f2166b9933b77ea85ec600
somnium421 <somnium421@kaist.ac.kr>
2021-05-25 01:45:39 +0900

    result error modified again

--- js/result.js
+++ js/result.js
@@ -306,9 +306,7 @@ $(document).ready(function() {
                     chats = doc.data();
                     console.log(chats);
                 })
-                if (chats != undefined) {
-                    for (var i=0; i<chats.chat.length; i++) drawChat(i);
-                }
+                for (var i=0; i<chats.chat.length; i++) drawChat(i);
             })
 
             if (meetings[meetingNumber].isPrivate) $('#is-private').append('<span class="text-muted" style="font-size: smaller;"><i class="fas fa-lock me-1"></i> 비공개</span>')
@@ -337,11 +335,12 @@ $(document).ready(function() {
                 }
                 for (var participant of meetings[meetingNumber].participants) {
                     if (!(Number(participant.id) in answered)) {
-                        $('#meeting-participants').append(` <a class="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title=${participant.name}>
+                        $('#meeting-participants').append(` <a class="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="${participant.name}">
                                                             <img src="${members[participant.id].img}" style="width:30px;height:30px;border-radius:70%;margin-bottom:2px;filter:brightness(0.3);opacity:0.4;"></img>
                                                         </a>`);
                     }
                 }
+                tooltipSet();
             })
 
             var meetingTags;
