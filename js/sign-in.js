71580df3ffcf4f523514cd88fab139cd1b826efa
heeju <heeju@imprimedicine.com>
2021-05-24 18:34:51 +0900

    signin validation

--- js/sign-in.js
+++ js/sign-in.js
@@ -36,11 +36,65 @@ $("#sign-in").click(()=>{
   userID = $("#userID").val();
   userPW = $("#userPW").val();
   var existID = false;
+  if (userID==''){
+    document.getElementById('userID').style = "border: 2px solid #E8ADAA";
+    document.getElementById('userPW').style = "border: ''";
+    var text = `<small id="signinHelp" class="form-text" style="color: red">생성할 아이디를 입력해주세요.</small>`
+    if ($("#signinHelp").length === 0) $("#userPW").parent().append(text);
+    else {
+        $("#signinHelp").remove();
+        $("#userPW").parent().append(text);}
+    return;
+  }
+  else if(userPW==''){
+    document.getElementById('userID').style = "border: 2px solid #E8ADAA";
+    document.getElementById('userPW').style = "border: ''";
+    var text = `<small id="signinHelp" class="form-text" style="color: red">생성할 비밀번호를 입력해주세요.</small>`
+    if ($("#signinHelp").length === 0) $("#userPW").parent().append(text);
+    else {
+        $("#signinHelp").remove();
+        $("#userPW").parent().append(text);}
+    return;
+  }
+  else if (userPW.length < 8){
+    document.getElementById('userPW').style = "border: 2px solid #E8ADAA";
+    document.getElementById('userID').style = "border: ''";
+    var text = `<small id="signinHelp" class="form-text" style="color: red">8자리 이상의 비밀번호를 만들어주세요.</small>`
+    if ($("#signinHelp").length === 0) $("#userPW").parent().append(text);
+    else {
+        $("#signinHelp").remove();
+        $("#userPW").parent().append(text);}
+    return;
+  }
+  else if (userPW.length > 21){
+    document.getElementById('userPW').style = "border: 2px solid #E8ADAA";
+    document.getElementById('userID').style = "border: ''";
+    var text = `<small id="signinHelp" class="form-text" style="color: red">20자리 이하의 비밀번호를 만들어주세요.</small>`
+    if ($("#signinHelp").length === 0) $("#userPW").parent().append(text);
+    else {
+        $("#signinHelp").remove();
+        $("#userPW").parent().append(text);}
+    return;
+  }
+
   for (var user of dbData) {
     if (user.id == userID) existID = true;
   }
-  if (existID) alert('이미 존재하는 아이디입니다')
+  if (existID) {
+    //alert('이미 존재하는 아이디입니다')
+    document.getElementById('userID').style = "border: 2px solid #E8ADAA";
+    document.getElementById('userPW').style = "border: ''";
+    var text = `<small id="signinHelp" class="form-text" style="color: red">이미 존재하는 아이디입니다</small>`
+    if ($("#signinHelp").length === 0) $("#userPW").parent().append(text);
+    else {
+        $("#signinHelp").remove();
+        $("#userPW").parent().append(text);}
+    return;
+  }
   else {
+    document.getElementById('userID').style = "border: ''";
+    document.getElementById('userPW').style = "border: ''";
+    $("#signinHelp").remove();
     /*
     db.collection('users').add({
       // family-code: familyCode,
